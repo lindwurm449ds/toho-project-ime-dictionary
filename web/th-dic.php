0e4e846c7d029d1352349fbc27c68165845336f5
shimosyan <nesher1121@yahoo.co.jp>
2019-07-20 10:29:24 +0900

    メタデータをyml化

--- web/th-dic.php
+++ web/th-dic.php
@@ -1,5 +1,9 @@
 <?php
-$param = [];
+require_once (dirname(__FILE__) . '/Spyc.php');
+ 
+$data = spyc_load_file('data.yml');
+
+$last = end($data['release-note']);
 ?>
 
 <!DOCTYPE html>
@@ -57,112 +61,17 @@ $param = [];
 				<section>
 					<h2 id="UPDATE">更新履歴</h2>
 					<dl class="update-history">
-						<dt>2019/07/18: <strong>R8-20190718</strong></dt>
-						<dd>
-							<ul>
-								<li>憑依華に対応</li>
-								<li>天空璋に対応</li>
-								<li>秘封ナイトメアダイアリーに対応</li>
-								<li>鬼形獣(体験版)に対応</li>
-								<li>The Grimoire of Usamiに対応</li>
-								<li>外來韋編を更新</li>
-								<li>茨歌仙を更新</li>
-								<li>鈴奈庵を更新</li>
-								<li>読み訂正[スペカ：積乱「見越し入道雲」] (みこしのにゅうどうぐも -> みこしにゅうどうぐも)</li>
-								<li>その他全体的にコメント内容を変更(表記揺れの修正など)</li>
-							</ul>
-						</dd>
-
-						<dt>2017/05/09: <strong>R7-20170509</strong></dt>
-						<dd>
-							<ul>
-								<li>憑依華(体験版)に一部対応</li>
-								<li>天空璋(体験版)に一部対応</li>
-								<li>燕石博物誌に対応</li>
-								<li>旧約酒場に対応</li>
-								<li>文果真報に対応</li>
-								<li>外來韋編に対応</li>
-								<li>三月精を更新</li>
-								<li>茨歌仙を更新</li>
-								<li>鈴奈庵を更新</li>
-								<li>単語追加[作品名:トルテルマジック]</li>
-								<li>単語追加[作品名:西方Project]</li>
-								<li>単語追加[曲名:8BIT MUSIC POWER FINAL/Mysterious Shrine]</li>
-								<li>単語追加[用語:尸解仙]</li>
-								<li>単語追加[用語:夢幻病]</li>
-								<li>単語追加[用語:酒呑童子]</li>
-								<li>単語追加[スペカ:ダブルスポイラー/お札「新聞拡張団調伏」]</li>
-								<li>誤記訂正[＠変換:＠さにーみるく/光を屈折を操る程度の能力 -> 光の屈折を操る程度の能力]</li>
-								<li>読み訂正[曲名:ダブルスポイラー/ネメシスの要塞] (めねしすのようさい -> ねめしすのようさい)</li>
-								<li>その他全体的にコメント内容を変更(表記揺れの修正など)</li>
-							</ul>
-						</dd>
-
-						<dt>2015/08/25: <strong>Release 6</strong></dt>
-						<dd>
-							<ul>
-								<li>弾幕アマノジャクに対応</li>
-								<li>深秘録に対応</li>
-								<li>紺珠伝に対応</li>
-								<li>その他全体的にコメント内容を変更(追加による文字数制限回避など)</li>
-							</ul>
-						</dd>
-
-						<dt>2013/10/23: <strong>Release 5</strong></dt>
-						<dd>
-							<ul>
-								<li>単語追加[永夜抄:「蓬莱人形」]</li>
-								<li>単語追加[萃夢想:酔符「鬼縛りの術」]</li>
-								<li>読み追加[Highly Responsive to Prayers] +(はいりーれすぽんしぶとぅぷれいやーず)</li>
-								<li>読み追加[古の冥界寺] +(いにしえのめいかいでら)</li>
-								<li>誤記訂正(リシッドパラダイス -> リジッドパラダイス)</li>
-								<li>誤記訂正(不思議な不思議な道具たち -> 不思議な不思議な道具達)</li>
-								<li>誤記訂正(雷矢「カゴウジサイクロン」 -> 雷矢「ガゴウジサイクロン」)</li>
-								<li>誤記訂正(雷矢「カゴウジトルネード」 -> 雷矢「ガゴウジトルネード」)</li>
-								<li>読み訂正[華霊「ディープルーティドバタフライ」] (でぃーぷるーでぃどばたふらい -> でぃーぷるーてぃどばたふらい)</li>
-								<li>読み訂正[宴会「死して全て大団円」] (ししてすべてだいえんだん -> ししてすべてだいだんえん)</li>
-								<li>読み訂正[桜符「桜吹雪地獄」] (さくらふぶきゆきじごく -> さくらふぶきじごく)</li>
-								<li>読み訂正[結界「動と静の均衡」] (どうとせいののきんこう -> どうとせいのきんこう)</li>
-								<li>読み訂正[参番勝負「延羽化弾幕変化」] (りょうせいかだんまくへんげ -> のべはかだんまくへんげ)</li>
-								<li>読み訂正[響符「平安の残響」] (へいあんのざんぎょう -> へいあんのざんきょう)</li>
-								<li>スペルカード辞書で [スウィープアサイド] の単語登録に失敗する不具合を修正</li>
-								<li>コメント文字数制限で登録に失敗する不具合を修正(全体的にコメントの内容を変更しています)</li>
-								<li>＠変換辞書を再構築</li>
-							</ul>
-						</dd>
-
-						<dt>2013/09/07: <strong>Release 4</strong></dt>
-						<dd>
-							<ul>
-								<li>輝針城に対応</li>
-								<li>心綺楼を更新</li>
-							</ul>
-						</dd>
-
-						<dt>2013/06/21: <strong>Release 3</strong></dt>
-						<dd>
-							<ul>
-								<li>心綺楼に対応(とりあえず)</li>
-								<li>輝針城(体験版)に対応(とりあえず)</li>
-								<li>茨歌仙を更新</li>
-								<li>鈴奈庵を更新</li>
-								<li>誤記訂正(妖精大戦争　～ Fairy War -> 妖精大戦争　～ Fairy Wars)</li>
-								<li>誤記訂正(天符「天の磐船よ天へ昇れ」 -> 天符「天の磐舟よ天へ昇れ」)</li>
-							</ul>
-						</dd>
-
-						<dt>2013/02/16: <strong>Release 2</strong></dt>
-						<dd>
-							<ul>
-								<li>MS-IME版でインポートできない不具合を修正</li>
-								<li>＠変換辞書でこいしのスペルが一部変換できない不具合を修正</li>
-								<li>Google 日本語入力版を作成</li>
-							</ul>
-						</dd>
-
-						<dt>2013/02/13: <strong>Release 1</strong></dt>
-						<dd>初版公開</dd>
-					</dl>
+						<?php 
+							foreach(array_reverse ($data['release-note']) as $item){
+								echo '<dt>'.$item['date'].': <strong>R'.$last['rev'].'-'.date('Ymd', strtotime($last['date'])).'</strong></dt>'."\n";
+								echo '<dd><ul>'."\n";
+								foreach ($item['note'] as $value) {
+									echo '<li>'.$value.'</li>'."\n";
+								}
+								echo '</ul></dd>'."\n";
+								echo "\n";
+							}
+						?>
 				</section>
 
 				<section>
@@ -170,11 +79,11 @@ $param = [];
 					<h3>収録作品</h3>
 					<dl class="compact-dl">
 						<dt>ゲーム</dt>
-							<dd>靈異伝, 封魔録, 夢時空, 幻想郷, 怪綺談, 紅魔郷, 妖々夢, 永夜抄, 萃夢想, 花映塚, 文花帖, 風神録, 緋想天, 地霊殿, 星蓮船, 非想天則, ダブルスポイラー, 妖精大戦争, 神霊廟, 心綺楼, 輝針城, 弾幕アマノジャク, 深秘録, 紺珠伝, 憑依華, 天空璋, ナイトメアダイアリー, 鬼形獣(体験版)</dd>
+							<dd><?php echo join(', ', $data['include-data']['game']) ?></dd>
 						<dt>書籍</dt>
-							<dd>文花帖, 求聞史紀, 求聞口授, 香霖堂, 三月精, 儚月抄, The Grimoire of Marisa, 茨歌仙, 鈴奈庵, 文果真報, 外來韋編, The Grimoire of Usami</dd>
+							<dd><?php echo join(', ', $data['include-data']['book']) ?></dd>
 						<dt>音楽CD</dt>
-							<dd>蓬莱人形, 蓮台野夜行, 夢違科学世紀, 卯酉東海道, 大空魔術, 未知の花　魅知の旅, 鳥船遺跡, 伊弉諾物質, 燕石博物誌, 旧約酒場, 単行本付属CD, 幺樂団の歴史1～5</dd>
+							<dd><?php echo join(', ', $data['include-data']['music-cd']) ?></dd>
 					</dl>
 					<p>その他、一部のゲスト参加作品の作品名などを収録しています。</p>
 
@@ -190,8 +99,8 @@ $param = [];
 						<section class="download">
 							<h3>for オープン拡張辞書</h3>
 							<ul>
-								<li>バージョン: <strong>R8-20190718</strong></li>
-								<li>更新日: 2019/07/18</li>
+								<li>バージョン: <strong><?php echo 'R'.$last['rev'].'-'.date('Ymd', strtotime($last['date'])) ?></strong></li>
+								<li>更新日: <?php echo $last['date'] ?></li>
 								<li>
 									<a href="https://cloud.9lab.jp/index.php/s/MrQv8Tb29D95ueM" title="東方Project辞書 for オープン拡張辞書">ダウンロード</a>
 									(ZIP形式, 517KB)
@@ -202,8 +111,8 @@ $param = [];
 						<section class="download">
 							<h3>for ATOK</h3>
 							<ul>
-								<li>バージョン: <strong>R8-20190718</strong></li>
-								<li>更新日: 2019/07/18</li>
+								<li>バージョン: <strong><?php echo 'R'.$last['rev'].'-'.date('Ymd', strtotime($last['date'])) ?></strong></li>
+								<li>更新日: <?php echo $last['date'] ?></li>
 								<li>
 									<a href="https://cloud.9lab.jp/index.php/s/qUXijUe85Mn2l7U" title="東方Project辞書 for ATOK">ダウンロード</a>
 									(ZIP形式, 1,519KB)
@@ -214,8 +123,8 @@ $param = [];
 						<section class="download">
 							<h3>for Google 日本語入力</h3>
 							<ul>
-								<li>バージョン: <strong>R8-20190718</strong></li>
-								<li>更新日: 2019/07/18</li>
+								<li>バージョン: <strong><?php echo 'R'.$last['rev'].'-'.date('Ymd', strtotime($last['date'])) ?></strong></li>
+								<li>更新日: <?php echo $last['date'] ?></li>
 								<li>
 									<a href="https://cloud.9lab.jp/index.php/s/3EbkhReDFFpS0Ph" title="東方Project辞書 for Google 日本語入力">ダウンロード</a>
 									(ZIP形式, 333KB)
@@ -226,8 +135,8 @@ $param = [];
 						<section class="download">
 							<h3>for MS-IME</h3>
 							<ul>
-								<li>バージョン: <strong>R8-20190718</strong></li>
-								<li>更新日: 2019/07/18</li>
+								<li>バージョン: <strong><?php echo 'R'.$last['rev'].'-'.date('Ymd', strtotime($last['date'])) ?></strong></li>
+								<li>更新日: <?php echo $last['date'] ?></li>
 								<li>
 									<a href="https://cloud.9lab.jp/index.php/s/WwYiDMHF91HZP67" title="東方Project辞書 for MS-IME">ダウンロード</a>
 									(ZIP形式, 330KB)
