1a57a8b6b5f1b8d44387426f487fc5392495cd91
shimosyan <nesher1121@yahoo.co.jp>
2019-07-21 18:20:01 +0900

    コメントの追加

--- tool/src/lib/Converter/Converter.ts
+++ tool/src/lib/Converter/Converter.ts
@@ -10,10 +10,19 @@ export abstract class Converter {
     });
   }
 
+  /**
+   * 出力する項目をセットする。
+   * @param {string[]} items 出力する項目を配列で指定
+   */
   public setItem = (items: string[]): void => {
     this.items = items;
   };
 
+  /**
+   * 指定したパスにファイルを書き出す。
+   * @param {string} path 出力するディレクトリ
+   * @param {string} ext  ファイルの拡張子
+   */
   public outputFile = (path: string, ext: string): void => {
     if (path.slice(-1) !== '/') {
       path = path + '/';
@@ -32,5 +41,9 @@ export abstract class Converter {
     }
   };
 
+  /**
+   * データオブジェクトを専用の文字列に変換する。
+   * @param {CsvElement[]} data 変換するデータ 
+   */
   protected abstract getConvertedData(data: CsvElement[]): string;
 }
