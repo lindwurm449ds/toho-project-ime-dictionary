c9b6f469a511cc7844883b742a9c5f75b8113e0a
shimosyan <nesher1121@yahoo.co.jp>
2019-07-27 16:52:39 +0900

    コメント追記

--- .circleci/upload_server.sh
+++ .circleci/upload_server.sh
@@ -12,6 +12,9 @@ echo $(apt-get install git-ftp)
 echo "Running: git ftp --version"
 echo $(git ftp --version)
 
+# git config git-ftp.user "$USER_NAME"
+# git config git-ftp.url "$URL"
+# git config git-ftp.password "$PASSWORD"
+
 echo "Running: git ftp push --remote-root $FTP_PATH"
-git ftp init
 git ftp push -u $FTP_USER_NAME -p $FTP_PASSWORD --remote-root $FTP_PATH $FTP_HOST
